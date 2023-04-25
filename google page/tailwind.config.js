0fbfac7e243762a5ae703c8241b8dcf4f42be090
shivam <rajputshivam478@gmail.com>
2023-04-25 11:30:30 +0530

    adding google-page task

---
+++ google page/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+
+        'primary-black': '#202124',
+        'secondary-black':'#303134',
+        'light-black':'#171717',
+      },
+    },
+  },
+  plugins: [],
+}
