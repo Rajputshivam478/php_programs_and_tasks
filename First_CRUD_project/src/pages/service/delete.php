3d40107a857e167352fc4849607e7a4943c17e64
shivam <rajputshivam478@gmail.com>
2023-05-19 15:58:58 +0530

    deleted date column added

--- First_CRUD_project/src/pages/service/delete.php
+++ First_CRUD_project/src/pages/service/delete.php
@@ -4,8 +4,11 @@ $user_id = "";
 if (isset($_REQUEST['delete'])) {
     $user_id = $_REQUEST['edit_user_id'];
 }
-$result = mysqli_query($conn, "DELETE FROM `user` WHERE `user_id`='$user_id'");
-$result = mysqli_query($conn, "DELETE FROM `profile` WHERE `user_id`='$user_id'");
-$result = mysqli_query($conn, "DELETE FROM `file` WHERE `user_id`='$user_id'");
+// $result = mysqli_query($conn, "DELETE FROM `user` WHERE `user_id`='$user_id'");
+// $result = mysqli_query($conn, "DELETE FROM `profile` WHERE `user_id`='$user_id'");
+// $result = mysqli_query($conn, "DELETE FROM `file` WHERE `user_id`='$user_id'");
+$date = date("y/m/d h:i:s");
+echo $date;
+$result = mysqli_query($conn, "UPDATE `user` SET `deleted_date`='$date' WHERE `user_id`='$user_id'");
 header('location: ../admin_page.php');
 ?>
